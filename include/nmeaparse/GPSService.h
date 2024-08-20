401e86a3140751cb976e6d638a77afa296f50e0a
Leo Heller <leo.heller@tum.de>
2024-08-20 16:10:57 +0200

    add new commands

--- include/nmeaparse/GPSService.h
+++ include/nmeaparse/GPSService.h
@@ -10,38 +10,46 @@
 #ifndef GPSSERVICE_H_
 #define GPSSERVICE_H_
 
-#include <string>
-#include <chrono>
-#include <functional>
+#include <nmeaparse/Event.h>
 #include <nmeaparse/GPSFix.h>
 #include <nmeaparse/NMEAParser.h>
-#include <nmeaparse/Event.h>
+
+#include <chrono>
+#include <functional>
+#include <string>
 
 namespace nmea {
 
 class GPSService {
-private:
+   private:
+    void read_PSRF150(const NMEASentence& nmea);
+    void read_GPGGA(const NMEASentence& nmea);
+    void read_GPGSA(const NMEASentence& nmea);
+    void read_GPGSV(const NMEASentence& nmea);
+    void read_GPRMC(const NMEASentence& nmea);
+    void read_GPVTG(const NMEASentence& nmea);
+    void read_GNGSA(const NMEASentence& nmea);
+    void read_GLGSV(const NMEASentence& nmea);
+    void read_GNGLL(const NMEASentence& nmea);
+    void read_GNRMC(const NMEASentence& nmea);
+    void read_GNVTG(const NMEASentence& nmea);
+    void read_GNGGA(const NMEASentence& nmea);
 
-	void read_PSRF150(const NMEASentence& nmea);
-	void read_GPGGA	(const NMEASentence& nmea);
-	void read_GPGSA	(const NMEASentence& nmea);
-	void read_GPGSV	(const NMEASentence& nmea);
-	void read_GPRMC	(const NMEASentence& nmea);
-	void read_GPVTG	(const NMEASentence& nmea);
+   public:
+    GPSFix fix;
 
-public:
-	GPSFix fix;
+    GPSService(NMEAParser& parser);
+    virtual ~GPSService();
 
-	GPSService(NMEAParser& parser);
-	virtual ~GPSService();
+    Event<void(bool)> onLockStateChanged;  // user assignable handler, called
+                                           // whenever lock changes
+    Event<void()>
+        onUpdate;  // user assignable handler, called whenever fix changes
 
-	Event<void(bool)> onLockStateChanged;		// user assignable handler, called whenever lock changes
-	Event<void()> onUpdate;						// user assignable handler, called whenever fix changes
-
-	void attachToParser(NMEAParser& parser);			// will attach to this parser's nmea sentence events
+    void attachToParser(NMEAParser& parser);  // will attach to this parser's
+                                              // nmea sentence events
 };
 
-
-}
+}  // namespace nmea
 
 #endif /* GPSSERVICE_H_ */
